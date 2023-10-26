465dba5e0cb02359eb5df07fd36c37d5cad36f82
flemimac <64695348+flemimac@users.noreply.github.com>
2023-10-26 20:32:06 +0200

    task

---
+++ task_6.py
@@ -0,0 +1,13 @@
+s = []
+age = int(input('Ваш возраст? '))
+s.append(age)
+gender = input('Ваш пол? ')
+s.append(gender)
+name = input('Ваше имя? ')
+s.append(name)
+city = input('Ваш город? ')
+s.append(city)
+room = input('В каком классе учитесь? ')
+s.append(room)
+
+print(s)
